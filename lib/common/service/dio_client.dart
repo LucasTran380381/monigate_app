d49c0f6f9ab3a8808c80f94e2a675ba7bc15ea47
lucas <Trannhan380381@gmail.com>
2022-04-11 19:42:54 +0700

    change server url (Tien server)

--- lib/common/service/dio_client.dart
+++ lib/common/service/dio_client.dart
@@ -3,6 +3,8 @@ import 'package:monigate_app/common/interceptors/logging_interceptor.dart';
 import 'package:monigate_app/common/interceptors/token_interceptor.dart';
 
 class DioClient {
-  static final instance = Dio(BaseOptions(baseUrl: 'https://monigate-capstone-ubuntu.azurewebsites.net/api'))
+  // 'https://monigate-capstone-ubuntu.azurewebsites.net/api'
+  // https://swp-monigate-ubuntu.azurewebsites.net/api
+  static final instance = Dio(BaseOptions(baseUrl: 'https://swp-monigate-ubuntu.azurewebsites.net/api'))
     ..interceptors.addAll([TokenInterceptor(), Logging()]);
 }
