2325568719919cc5a82c964988b12ca5c97d5dd1
lucas <Trannhan380381@gmail.com>
2022-04-27 11:17:03 +0700

    get tracing state

--- lib/contact_tracing/logic/tracing_provider.dart
+++ lib/contact_tracing/logic/tracing_provider.dart
@@ -1,4 +1,5 @@
 import 'package:background_fetch/background_fetch.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:monigate_app/contact_tracing/services/tracing_service.dart';
 
@@ -12,9 +13,17 @@ final tracingProvider = StateNotifierProvider<TracingNotifier, TracingState>((re
 });
 
 class TracingNotifier extends StateNotifier<TracingState> {
-  TracingNotifier(this._ref) : super(TracingState.stopped);
+  TracingNotifier(this._ref) : super(TracingState.stopped) {
+    _getTracingStatus();
+  }
 
   final Ref _ref;
+
+  _getTracingStatus() async {
+    final result = await const MethodChannel('dev.lucas.tracing').invokeMethod('getTracingServiceStatus');
+    print('tracing result: $result');
+    state = result == 'On' ? TracingState.running : TracingState.stopped;
+  }
 
   _startService() {
     _ref.read(tracingServiceProvider).startBle();
